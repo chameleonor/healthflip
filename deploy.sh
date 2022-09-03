f5f88743cf80d9944f6d3e762770bd0868237fd9
chameleonor <guilherme.or2013@gmail.com>
2022-09-03 11:32:21 -0300

    deploying to production

--- deploy.sh
+++ deploy.sh
@@ -9,16 +9,11 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
 git init
 git checkout -b main
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
 git push -f git@github.com:daniloak/daniloak.github.io.git production
-# git push -f git@github.com:daniloak/healthflip.git main:gh-pages
 
 cd -
